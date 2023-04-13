0b4ae66db34274351feb599adfa56e67c4cc5e98
sergiozc <sergiozapata25@gmail.com>
2023-04-13 19:34:29 +0200

    adding graphics

--- beamformer_p4/beamformer.m
+++ beamformer_p4/beamformer.m
@@ -33,7 +33,7 @@ xc_out = zeros(L_signal,N);
 
 %obtenemos los pesos por medio de nuestra función auxiliar
 w = pesos(tn, freq);
-
+XOUT = zeros(129, 1);
 
 %Para realizar el trabajo, usaremos un doble bucle
 for ntram = 1:Ntramas 
@@ -45,9 +45,11 @@ for ntram = 1:Ntramas
         Xn = Xn(1:129);                 %Tomamos las componentes de frecuencia de 0 a Fs/2 (Fs/2 = 8 kHz)s     
         Xn = Xn .* w(:,c);              %Multiplicamos por los pesos correspondientes
         
-        %NOTA: Si haces un plot aquí del espectro se ve una cosa que no se
-        %si esta bien
 
+        
+        
+        
+        % ESTO CREO QUE VA FUERA DEL FOR (DESPUÉS DEL SUM)
         %Realizamos la simetrización para practicar la transformada inversa
         XOUT = cat(1, Xn, Xn(end:-1:2)); %NO SE SI ESTÁ BIEN LA DIMENSIÓN
 
@@ -55,14 +57,16 @@ for ntram = 1:Ntramas
         xout = ifft(sqrt(win).*XOUT);
         xout = real(xout);
         
-        %Sacamos el resultado final
+        %Sacamos el resultado final (concatenación de las tramas)
         xc_out(iter:iter + Ltrama, c) = xc_out(iter:iter + Ltrama, c) + xout;
 
     end
     
+    
     iter = iter + 127; %Actualizamos el iterador
 
 end
+
 
 %% comprobaciones
 
@@ -71,7 +75,7 @@ plot(xc_out(:,3))
 
 xc_out_all = zeros(L_signal,1);
 
-%Sumando todos
+%Sumando todos (SUM)
 
 for i =1:7
 
@@ -84,4 +88,54 @@ plot(xc_out_all)
 title('suma de todos ')
 
 
+%% Comprobación beamformer
+
+% VARIABLES PARA REPRESENTAR
+theta = linspace(0, pi, 200); % Barrido en theta
+theta_polar = linspace(0, 2*pi, 400); % Barrido representación polar
+theta_surf = linspace(0, 2*pi, 129); % Barrido en theta
+
+
+
+% VISUALIZACIÓN DE LOS RETARDOS
+figure(1)
+stem(tn)
+title('Retardos')
+ylabel('tiempo (s)')
+xlabel('N sensor')
+% (La señal llega antes al último sensor).
+
+
+% DIRECTIVIDAD PARA 1 FRECUENCIA
+index_freq1 = 17; % 1kHz
+D_1kHz = calcula_Dteo(w, freq, index_freq1, d, Vprop, theta);
+index_freq2 = 129; % 8kHz
+D_8kHz = calcula_Dteo(w, freq, index_freq2, d, Vprop, theta);
+
+figure(2);
+polarplot(theta_polar, D_1kHz);
+hold on
+polarplot(theta_polar, D_8kHz);
+title('Directividad teórica');
+legend('f = 1kHz', 'f = 8kHz');
+
+% DIRECTIVIDAD EN FUNCIÓN DE LA FRECUENCIA
+Df = calcula_Df(w, freq, d, Vprop, theta_surf);
+figure(3);
+surf(freq, rad2deg(theta_surf), Df);
+xlabel('f(Hz)');
+ylabel('phi');
+zlabel('D(f, phi)');
+
+figure(4);
+pcolor(freq, rad2deg(theta_surf), abs(Df.'));
+shading interp;
+colorbar;
+xlabel('Frecuencia (Hz)');
+ylabel('φ(grados)');
+title('Directividad en función de la frecuencia y ángulo');
+% Se aprecia como a frecuencias más altas, el ancho del lóbulo principal
+% disminuye (se hace más directivo). delta = 1/(f*L)
+% Se ve también que a partir de un determinado valor de frecuencia,
+% aparecen más rizados (o lóbulos secundarios).
 
